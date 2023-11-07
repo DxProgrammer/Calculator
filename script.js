a3ce57532a8a84272ef5f353575f26568f7b7018
ashokthakurhisar <132098149+ErDeepak1910@users.noreply.github.com>
2023-11-06 16:07:07 -0800

    Calculater

---
+++ script.js
@@ -0,0 +1,29 @@
+(function(){
+
+let screen=document.querySelector(".screen")
+let button=document.querySelectorAll(".btn")
+let clear=document.querySelector(".btn-clear")
+let equal=document.querySelector(".btn-equal")
+
+button.forEach(function(button){
+    button.addEventListener('click',function(e){
+        let value=e.target.dataset.num;
+        screen.value+=value
+    })
+});
+equal.addEventListener('click',(e)=>{
+    if(screen.value===""){
+        screen.value="please enter";
+    }
+    else{
+        let answer=eval(screen.value);
+        screen.value=answer;
+    }
+})
+
+clear.addEventListener('click',function(e){
+    screen.value="";
+
+})
+
+})();
